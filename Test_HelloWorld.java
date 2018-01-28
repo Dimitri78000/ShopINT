dc7861c10866316c68fc3170f7f491d84b1bacd7
YannisTevissen <33493734+YannisTevissen@users.noreply.github.com>
2018-01-28 22:57:03 +0100

    Update Test_HelloWorld.java

--- Test_HelloWorld.java
+++ Test_HelloWorld.java
@@ -3,6 +3,7 @@ public class Test_HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Test de GitHub 25/01/18 - Hello World !");
 		System.out.println("Dimitri a réussi a modifier le fichier");
+		System.out.println("Yannis a réussi a modifier le fichier");
 
 	}
 

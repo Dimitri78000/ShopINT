bc6d0164903a9fe20e6fa4df945cc3158b39dcf1
evalegal <35901932+evalegal@users.noreply.github.com>
2018-01-28 22:59:37 +0100

    Update Test_HelloWorld.java

--- Test_HelloWorld.java
+++ Test_HelloWorld.java
@@ -4,6 +4,8 @@ public class Test_HelloWorld {
 		System.out.println("Test de GitHub 25/01/18 - Hello World !");
 		System.out.println("Dimitri a réussi a modifier le fichier");
 		System.out.println("Yannis a réussi a modifier le fichier");
+		System.out.println ("Eva a réussi a modifier le fichier");
+	
 
 	}
 

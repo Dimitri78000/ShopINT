deb574b0652977f7fcb0db26c7e05745b3b140d2
HuguetPaul <35903346+HuguetPaul@users.noreply.github.com>
2018-01-29 08:50:10 +0100

    Update Test_HelloWorld.java

--- Test_HelloWorld.java
+++ Test_HelloWorld.java
@@ -5,6 +5,7 @@ public class Test_HelloWorld {
 		System.out.println("Dimitri a réussi a modifier le fichier");
 		System.out.println("Yannis a réussi a modifier le fichier");
 		System.out.println ("Eva a réussi a modifier le fichier");
+		System.out.println ("Paul a réussi a modifier le fichier");
 		
 		while (true) {
 			System.out.println("Simon est un crétin !");

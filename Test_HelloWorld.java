5c2dc2c52fc0a15e6b34feb1a117bfbeb3afdaed
Dimitri78000 <dimi_du_24@hotmail.fr>
2018-01-29 22:22:44 +0100

    Un test

--- Test_HelloWorld.java
+++ Test_HelloWorld.java
@@ -1,8 +1,9 @@
-
+﻿
 public class Test_HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Test de GitHub 25/01/18 - Hello World !");
 		System.out.println("Dimitri a réussi a modifier le fichier");
+		System.out.println("Dimitri a réussi a modifier le fichier via l'application GitHub Desktop");
 		System.out.println("Yannis a réussi a modifier le fichier");
 		System.out.println ("Eva a réussi a modifier le fichier");
 	

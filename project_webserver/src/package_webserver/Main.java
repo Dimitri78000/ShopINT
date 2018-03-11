d6f4ba13fa6010d101e4620bd57877c15b702eab
Dimitri78000 <dimi_du_24@hotmail.fr>
2018-03-11 15:15:12 +0100

    e

---
+++ project_webserver/src/package_webserver/Main.java
@@ -0,0 +1,28 @@
+package package_webserver;
+import package_shopping.*;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception 
+	{
+		System.out.println("Hello World Server!");
+		Magasin Carrefour = new Magasin();
+
+		int port = 4123;
+	    ServerSocket server = new ServerSocket(port);
+	    while(true) {
+	        try {
+	          new Server(server.accept()).handle();
+	        } catch(IOException e) {
+	          System.out.println(e.getMessage());
+	          e.printStackTrace();
+	        }
+	      }    
+	    
+	}
+
+}
+
--- project_webserver/src/package_webserver/Server.java
+++ project_webserver/src/package_webserver/Server.java
@@ -1,5 +1,61 @@
 package package_webserver;
+import package_shopping.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
 
 public class Server {
+  private Socket socket;
+  private BufferedReader reader;
+  private PrintStream out;
+  
+  public Server(Socket socket) throws IOException {
+    this.socket = socket;
+    reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    out = new PrintStream(socket.getOutputStream());
+  }
 
+  public void handle() throws IOException {
+    String method = reader.readLine();
+    String[] cmd = method.split(" ");
+    String location = cmd[1];
+
+    location = location.replace("%20", " ");
+    String[] tokens = location.split("/", 0);
+
+    out.println("HTTP/1.1 200 OK");
+    out.println("");
+    out.println("<html><body>");
+    out.println("Hello World interface utilisateur"); 
+    
+    System.out.println("Request " + location + " => " + tokens.length + " tokens");
+    
+   /* try {
+      switch(tokens.length) {
+      case 0: 
+        out.println(db.asHTML()); 
+        break;
+      case 3:
+        String name = tokens[1];
+        int year = Integer.parseInt(tokens[2]);
+        Recommendations recommendations = db.lookup(name, year);
+
+        out.println("Recommendations pour le film " + name + " (" + year + ") :");
+        out.println(recommendations.asHTML());
+        out.println("<p/><a href='/'>Back</a>");
+        break;
+      default:
+        throw new BadRequestException("bad request: " + location);
+      }
+    } catch(BadRequestException|FilmDoesNotExistsException e) {
+      out.println(e.getMessage());
+      out.println("<p/><a href='/'>Back</a>");      
+    }
+    */
+    out.println("</body></html>");
+    socket.close();
+  }
 }

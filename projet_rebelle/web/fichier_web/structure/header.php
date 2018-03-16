984692e159428abafa7c6295b8dbb10031b24954
Dimitri78000 <dimi_du_24@hotmail.fr>
2018-03-16 18:09:09 +0100

    rebelle

---
+++ projet_rebelle/web/fichier_web/structure/header.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+
+		<!-- Style -->
+		<link rel="stylesheet" href="fichier_style/style.css" />
+		
+		<!-- Favicon -->
+		<link rel="apple-touch-icon" sizes="57x57" href="fichier_autres/favicon/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="fichier_autres/favicon/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="fichier_autres/favicon/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="fichier_autres/favicon/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="fichier_autres/favicon/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="fichier_autres/favicon/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="fichier_autres/favicon/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="fichier_autres/favicon/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="fichier_autres/favicon/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="fichier_autres/favicon/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="fichier_autres/favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="fichier_autres/favicon/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="fichier_autres/favicon/favicon-16x16.png">
+		<link rel="manifest" href="fichier_autres/favicon/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="fichier_autres/favicon/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+
+
+
+		<title> ShopINT - Carrefour Evry </title>
+	</head>
+<body> 
+
+	<header>
+
+		<truc class="menu_header">
+			<a  class="onglet_header" href="index.php"> <img id="image_header" src="autres/favicon/android-icon-72x72" /> </a>             
+				<a class="onglet_header" href="connection.php"> Connection </a> <a class="espace"> es </a>   
+				<a class="onglet_header" href="course.php"> Course </a> <a class="espace"> es </a>               
+				<a class="onglet_header" href="plan.php"> Plan </a> <a class="espace"> es </a>                   
+				<a class="onglet_header" href="contact.php"> Contact </a> <a class="espace"> es </a>
+
+			<!-- STYLE DEBUT -->       
+			<style> 
+			#image_header{ vertical-align: bottom;}
+			.menu_header{
+				font-size : 1.3em;
+				text-align : center;
+			 	border: 2px black solid;
+			 	border-radius: 4px;
+			 	width:500px;
+				padding: 5px 20px 5px 20px;
+				margin: 0px 20px 0px 20px;
+			}
+			.espace { color: rgba(0,0,0,0);}
+			.onglet_header { text-decoration: none; }
+			</style> 
+			<!-- STYLE FIN -->
+
+		</truc>  
+ 
+	</header> 
+
+
---
+++ projet_rebelle/web/index.php
@@ -0,0 +1,5 @@
+<?php include('fichier_web/structure/header.php'); ?>
+<br/> <a> Bienvenue sur l'application ShopINT - Carrefour Evry ! </a>
+
+
+<?php include('fichier_web/structure/footer.php'); ?>
